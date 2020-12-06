37a1ae96bdf194c8bf04786460af56899b575b90
Lucas Kimanzi <lkimanzi@hotmail.com>
2020-12-06 16:10:20 -0500

    post quiz push_used_built_in_functions_instead_of_loops

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function reverseString(str) {
-   let  word = "";
-    for (let i =      ) {
+/*function reverseString(str) {
+  // let  word = "";
+    //for (let i =      ) {
         
     }
     return word;
-}
+}*/
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}it 
+
+//To call function reversString("hello")
